c1a4b067b43220ad8c7b3e675ceeff01db0d027d
liesmithson <isamqd@gmail.com>
2016-08-24 12:32:27 -0500

    hacer un cambio

--- hola.c
+++ hola.c
@@ -1,6 +1,8 @@
 #include <iostream>
 int main(){
+    printf("Soy el papi");
     printf("Está cool");
+    printf(":v");
     printf(">:(");
     printf ("modificando el archivo \n ");
     return 0;
